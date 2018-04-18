313a5fbd6e27be89617329a673e667314c72dd45
ou-ansgar <Ansgar.Goeb@my.open.ac.uk>
2018-04-18 16:12:47 +0200

    documentation Line

--- Paint/src/groupd/paint/Line.java
+++ Paint/src/groupd/paint/Line.java
@@ -6,20 +6,26 @@ import java.awt.Graphics;
 /**
  * 
  * @author ansgar.goeb
- *
+ * 
+ *         Shape that draws a line
  */
 public class Line extends BaseShape {
 
+	/**
+	 * constructor of a line the start coordinates and the color of the lines must
+	 * be set
+	 */
 	public Line(int aStartX, int aStartY, Color aColor) {
 		super(aStartX, aStartY, aColor);
 	}
 
+	/**
+	 * methods to draw aline
+	 */
 	@Override
 	public void draw(Graphics g) {
 
 		g.setColor(getColor());
 		g.drawLine(getStartX(), getStartY(), getEndX(), getEndY());
-
 	}
-
 }
