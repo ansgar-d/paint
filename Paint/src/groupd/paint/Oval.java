3b716d381964622650c6f41ab179bbfdd8fb0c72
ou-ansgar <Ansgar.Goeb@my.open.ac.uk>
2018-04-18 16:20:13 +0200

    comments Oval

--- Paint/src/groupd/paint/Oval.java
+++ Paint/src/groupd/paint/Oval.java
@@ -8,31 +8,39 @@ import java.awt.Graphics2D;
 /**
  * 
  * @author ansgar.goeb
- *
+ * 
+ * Class paints an Oval
  */
 public class Oval extends BoundedShape {
 
-	
-		public Oval(int aStartX, int aStartY, Color aColor, Color aFillColor) {
-			super(aStartX, aStartY, aColor, aFillColor);
-		}
-
-		
-		@Override
-		public void draw(Graphics g) {
-		
-			Graphics2D g2 = (Graphics2D) g;
-			
-			if (getFillColor() != null) {
-
-				g2.setColor(getFillColor());
-				g2.fillOval(getX() , getY() ,getWidth(), getHeight() );
-			}
-			
-			g2.setStroke(new BasicStroke(2));
-			g2.setColor(getColor());
-
-			g2.drawOval(getX(), getY(),getWidth(), getHeight());
-			
-		}
+    /**
+     * 	
+     * Constructor of an Oval. The start start coordinates of
+     * the object, the color of the outline and the fill color
+     * needs to be set.
+     */
+	public Oval(int aStartX, int aStartY, Color aColor, Color aFillColor) {
+		super(aStartX, aStartY, aColor, aFillColor);
 	}
+
+
+	/**
+	 * draws an Oval
+	 */
+	@Override
+	public void draw(Graphics g) {
+
+		Graphics2D g2 = (Graphics2D) g;
+
+		if (getFillColor() != null) {
+
+			g2.setColor(getFillColor());
+			g2.fillOval(getX(), getY(), getWidth(), getHeight());
+		}
+
+		g2.setStroke(new BasicStroke(2));
+		g2.setColor(getColor());
+
+		g2.drawOval(getX(), getY(), getWidth(), getHeight());
+	}
+}
